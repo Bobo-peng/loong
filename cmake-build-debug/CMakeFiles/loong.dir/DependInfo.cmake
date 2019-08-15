1bfc1f435641ae8261fd6315d408d106d04849ab
Bobo-peng <3964007901@qq.com>
2019-08-15 15:00:27 +0800

    code-push

---
+++ cmake-build-debug/CMakeFiles/loong.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/didi/Documents/workspace/loong/event.cpp" "/Users/didi/Documents/workspace/loong/cmake-build-debug/CMakeFiles/loong.dir/event.cpp.o"
+  "/Users/didi/Documents/workspace/loong/log.cpp" "/Users/didi/Documents/workspace/loong/cmake-build-debug/CMakeFiles/loong.dir/log.cpp.o"
+  "/Users/didi/Documents/workspace/loong/main.cpp" "/Users/didi/Documents/workspace/loong/cmake-build-debug/CMakeFiles/loong.dir/main.cpp.o"
+  "/Users/didi/Documents/workspace/loong/network.cpp" "/Users/didi/Documents/workspace/loong/cmake-build-debug/CMakeFiles/loong.dir/network.cpp.o"
+  "/Users/didi/Documents/workspace/loong/server.cpp" "/Users/didi/Documents/workspace/loong/cmake-build-debug/CMakeFiles/loong.dir/server.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/loong.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/loong.dir/main.cpp.o"
+  "CMakeFiles/loong.dir/server.cpp.o"
+  "CMakeFiles/loong.dir/event.cpp.o"
+  "CMakeFiles/loong.dir/network.cpp.o"
+  "CMakeFiles/loong.dir/log.cpp.o"
+  "loong.pdb"
+  "loong"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/loong.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
